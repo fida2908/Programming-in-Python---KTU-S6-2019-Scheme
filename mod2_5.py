7079c367f4b0f9e74d7eb0937c0a2ed3a81d3742
Fida K A <157485577+fida2908@users.noreply.github.com>
2025-04-15 11:16:39 +0530

    Add

---
+++ mod2_5.py
@@ -0,0 +1,12 @@
+#Create a function min_max0 that takes n numbers as list argument and return the smallest and largest numbers.
+
+def min_max(list):
+    small = min(list)
+    large = max(list)
+    return small,large
+
+list = [1,2,3 -5, 6 ,900, -900]
+mini = min_max(list)
+print('Smallest and Largest: ',mini )
+
+    
